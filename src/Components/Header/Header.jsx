ec21bf6b1d992eebf4b8b2ea41b3b0660f500d21
nurullah91 <mdnurullah.ptk@gmail.com>
2023-03-30 23:34:29 +0600

    initial setup done with header compo

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div>
+            Hello Form Header jsx
+        </div>
+    );
+};
+
+export default Header;
