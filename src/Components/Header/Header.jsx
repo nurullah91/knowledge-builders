56316adb921caba9ae47c8ca4d3f920d44febf42
nurullah91 <mdnurullah.ptk@gmail.com>
2023-03-30 23:51:27 +0600

    header added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,8 +3,9 @@ import "./Header.css"
 
 const Header = () => {
     return (
-        <div>
-            Hello Form Header jsx
+        <div className='header-container'>
+           <h2>Knowledge Builders</h2>
+           <img src="https://images.unsplash.com/photo-1654110455429-cf322b40a906?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" />
         </div>
     );
 };
